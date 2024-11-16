bf49509c24c7a13967af64d6525926c8edfbf74d
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-16 00:48:33 -0300

    Create imprima os números pares de 0 a 100.java

---
+++ exercicios/imprima os números pares de 0 a 100.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int soma = 0;
+        for(byte i = 1; i <= 100; i++){
+           if(i % 2 == 0){
+               System.out.println(i + " par");
+           }
+        }
+    }
+}

4d86acb648b06fa1a1a6d24a907a658ebe4faeba
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-16 00:46:11 -0300

    Create imprima os números de 1 a 10 em ordem crescente e depois em ordem decrescente.java

---
+++ exercicios/imprima os números de 1 a 10 em ordem crescente e depois em ordem decrescente.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int soma = 0;
+        for(byte i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+
+        for(byte i = 10; i >= 1; i--){
+            System.out.println(i);
+        }
+    }
+}

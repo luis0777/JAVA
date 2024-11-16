086245a868760c2bc01487c475e0c168b040b227
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-16 00:34:56 -0300

    Create imprima se um número é positivo ou negativo baseado no loop.java

---
+++ exercicios/imprima se um número é positivo ou negativo baseado no loop.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        for(byte i = -10; i <= 30; i++){
+            if(i < 0){
+                System.out.println(i + " Negativo");
+            }else{
+                System.out.println(i + " Positivo");
+            }
+        }
+    }
+}

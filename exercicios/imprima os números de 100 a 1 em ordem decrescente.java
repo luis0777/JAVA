5f195f8f551bc2cf611eed18630401fd47fa28a2
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-16 00:43:14 -0300

    Create imprima os números de 100 a 1 em ordem decrescente.java

---
+++ exercicios/imprima os números de 100 a 1 em ordem decrescente.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int soma = 0;
+        for(byte i = 100; i >= 1; i--){
+            System.out.println(i);
+        }
+    }
+}

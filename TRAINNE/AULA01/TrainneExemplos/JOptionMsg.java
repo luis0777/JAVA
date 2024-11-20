c5b146a5a3af7c13a1070f669657a8bbc73bcce3
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-19 21:51:40 -0300

    Create JOptionMsg.java

---
+++ TRAINNE/AULA01/TrainneExemplos/JOptionMsg.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+
+public class JOptionMsg{
+    public  static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "Erro ao processar a solicitação!", "Error", JOptionPane.ERROR_MESSAGE);
+    }
+}

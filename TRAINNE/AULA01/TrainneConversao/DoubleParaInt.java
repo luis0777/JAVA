bc09be8b7fb701660ec5f0795dbedc549d156a35
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-19 23:56:10 -0300

    Create DoubleParaInt.java

---
+++ TRAINNE/AULA01/TrainneConversao/DoubleParaInt.java
@@ -0,0 +1,7 @@
+public class DoubleParaInt{
+    public static void main(String[] args){
+        double numeroDouble = 123.45;
+        int numeroInt = (int) numeroDouble;
+        System.out.println("double: " + numeroDouble + " -> int: " + numeroInt);
+    }
+}

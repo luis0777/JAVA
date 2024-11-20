872aa9068f8b65c7519f10e4de487635bf886060
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-19 23:47:52 -0300

    Create DoubleParaString.java

---
+++ TRAINNE/AULA01/TrainneConversao/DoubleParaString.java
@@ -0,0 +1,7 @@
+public class DoubleParaString{
+    public static void main(String[] args){
+        double numeroDouble = 789.01;
+        String numeroStr = Double.toString(numeroDouble);
+        System.out.println("Double: " + numeroDouble + " -> String: " + numeroStr);
+    }
+}

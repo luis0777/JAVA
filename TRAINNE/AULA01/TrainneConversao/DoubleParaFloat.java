b814dd9035ac5fb3a9dfd1c4a087be1de30b6926
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-19 23:40:53 -0300

    Create DoubleParaFloat.java

---
+++ TRAINNE/AULA01/TrainneConversao/DoubleParaFloat.java
@@ -0,0 +1,7 @@
+public class DoubleParaFloat{
+    public static void main(String[]args){
+        double numeroDouble = 123.4567896544;
+        float numeroFloat = (float) numeroDouble;
+        System.out.println("Double: " + numeroDouble + " -> float: " + numeroFloat);
+    }
+}

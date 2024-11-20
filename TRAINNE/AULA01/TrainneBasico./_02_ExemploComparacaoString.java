f11dff3b3c7cc68ab0a1c809729d37084f62f41d
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-20 00:23:17 -0300

    Create _02_ExemploComparacaoString.java

---
+++ TRAINNE/AULA01/TrainneBasico./_02_ExemploComparacaoString.java
@@ -0,0 +1,23 @@
+
+public class _02_ExemploComparacaoString {
+    public static void main(String[] args) {
+
+        String str1 = "Java";
+        String str2 = "Java";
+        
+        
+        str1 = "Dart";
+
+        System.out.println(str1);
+
+        // if ("Java" == "Java") {
+        // if (str1 == "Java") {
+        // if (str1 == str2) {
+        // if (str1.equals(str2)) {
+        // if (str1.equalsIgnoreCase(str2)) {
+        //     System.out.println("As strings sao Iguais!");
+        // } else {
+        //     System.out.println("As strings sao #Diferentes#");
+        // }
+    }
+}

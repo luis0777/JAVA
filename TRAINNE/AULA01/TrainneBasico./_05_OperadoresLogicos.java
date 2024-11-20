6585c14f62bd91030a68a4a5746c344c25234031
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-20 00:24:54 -0300

    Create _05_OperadoresLogicos.java

---
+++ TRAINNE/AULA01/TrainneBasico./_05_OperadoresLogicos.java
@@ -0,0 +1,12 @@
+public class _05_OperadoresLogicos {
+
+    public static void main(String[] args) {
+        boolean a = true;
+
+        if (!a) {
+            System.out.println("Saida==> " + a);
+        } else {
+            System.out.println("Saida==> " + !a);
+        }
+    }
+}

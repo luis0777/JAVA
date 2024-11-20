a3172daf1d0dcc5d6a61c5016f0ade6db45d0a1f
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-19 21:30:15 -0300

    Create Comentario.java

---
+++ TRAINNE/AULA01/TrainneExemplos/Comentario.java
@@ -0,0 +1,10 @@
+public class Comentarios{
+
+    public static void main(String args[]){
+        int matricula = 1089, idade = 20;
+
+        double dolar = 5.77;
+
+        System.out.println("Matricula: " + matricula + "\nIdade: " + idade + "\nDolar hoje nov/2024: " + dolar);
+    }
+}

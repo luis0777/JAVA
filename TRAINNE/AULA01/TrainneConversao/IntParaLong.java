6f9218d5c76ef3cab2e5f2a7107384e7448f52a1
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-20 00:03:44 -0300

    Create IntParaLong.java

---
+++ TRAINNE/AULA01/TrainneConversao/IntParaLong.java
@@ -0,0 +1,7 @@
+public class IntParaLong{
+    public static void main(String[]args){
+        int numeroInt = 12345;
+        long numeroLong = (long) numeroInt;
+        System.out.println("int: " + numeroInt + " -> long: " + numeroLong);
+    }
+}

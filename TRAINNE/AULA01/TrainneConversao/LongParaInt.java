674fe55c255dfadcd364ec274de296ad179ab8d3
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-20 00:12:19 -0300

    Create LongParaInt.java

---
+++ TRAINNE/AULA01/TrainneConversao/LongParaInt.java
@@ -0,0 +1,7 @@
+public class LongParaInt{
+    public static void main(String[]args){
+        long numeroLong = 123456789L;
+        int numeroInt = (int) numeroLong;
+        System.out.println("long: " + numeroLong + " -> int: "+ numeroInt);
+    }
+}

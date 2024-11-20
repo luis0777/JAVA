a6c3b41e83a30d0b572695c7c360156fe1099285
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-19 21:33:17 -0300

    Create TestePrimeiraClasse.java

---
+++ TRAINNE/AULA01/TrainneExemplos/TestePrimeiraClasse.java
@@ -0,0 +1,9 @@
+public class TestePrimeiraClasseJava{
+    public static void main(String []args){
+        int valor;
+        String info1, info2;
+        info1 ="Teste da linguagem";
+        info2="JAVA";
+        System.out.println(info1+info2);
+    }
+}

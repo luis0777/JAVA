5fb8715ee9b24bbaee2a0bf169a0ad2945e1b609
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-19 23:52:36 -0300

    Create FloatParaDouble.java

---
+++ TRAINNE/AULA01/TrainneConversao/FloatParaDouble.java
@@ -0,0 +1,7 @@
+public class FloatParaDouble{
+    public static void main(String[] args){
+        float numeroFloat = 12.34F;
+        double numeroDouble = (double) numeroFloat;
+        System.out.println("float: " + numeroFloat + " -> double: " + numeroDouble);
+    }
+}

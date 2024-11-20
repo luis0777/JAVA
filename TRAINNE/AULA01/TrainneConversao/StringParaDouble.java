e11a1be5ca4e533b415589a9c8deb34282478a5c
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-20 00:15:53 -0300

    Create StringParaDouble.java

---
+++ TRAINNE/AULA01/TrainneConversao/StringParaDouble.java
@@ -0,0 +1,7 @@
+public class StringParaDouble{
+    public static void main(String [] args){
+        String numeroStr = "123.45";
+        double numeroDouble = Double.parseDouble(numeroStr);
+        System.out.println("String: " + numeroStr + " -> double: " + numeroDouble);
+    }
+}

0b7486d87020655e044934828878f8fb1d6c4740
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-19 21:42:12 -0300

    Create TiposPrimitivos.java

---
+++ TRAINNE/AULA01/TrainneExemplos/TiposPrimitivos.java
@@ -0,0 +1,16 @@
+public class TiposPrimitivos{
+    public static void main(String args[]){
+        char sexo = 'f';
+        byte idade = 16;
+        short codigo = 256;
+        float nota = 9.4f;
+        int alunos = 100, classes = 10;
+        long habitantes = 8202023;
+        double dolar = 5.77;
+        boolean alternativa = false;
+
+        System.out.println("\nSexo: " + sexo + " idade: " + idade + " Codigo: " + codigo);
+        System.out.println("Nota: " + nota + " alunos: " + alunos + " classe: " + classes);
+        System.out.println("Habiantes: " + habitantes + " dolar: " + dolar + " alternativa: " + alternativa);
+    }
+}

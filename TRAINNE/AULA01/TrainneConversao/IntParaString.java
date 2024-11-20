8747f9d29191eda45e02d4aca9a13ec63a4f4660
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-20 00:06:48 -0300

    Create IntParaString.java

---
+++ TRAINNE/AULA01/TrainneConversao/IntParaString.java
@@ -0,0 +1,7 @@
+public class IntParaString{
+    public static void main(String[] args){
+        int numeroInt = 456;
+        String numeroStr = Integer.toString(numeroInt);
+        System.out.println("int: " + numeroInt + " -> String: " + numeroStr);
+    }
+}

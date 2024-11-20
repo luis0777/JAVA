ff202cf955acda103a9569db9dbe67ef0f1a5a05
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-20 00:22:50 -0300

    Create _01_App.java

---
+++ TRAINNE/AULA01/TrainneBasico./_01_App.java
@@ -0,0 +1,8 @@
+public class _01_App {
+    // public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+        // double a= 3.14;
+        // System.out.println(a);
+    }
+}

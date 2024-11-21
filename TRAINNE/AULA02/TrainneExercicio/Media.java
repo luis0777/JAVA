475621156ead69e7387dda1b56ffdd0228695c2f
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-21 11:47:04 -0300

    Create Media.java

---
+++ TRAINNE/AULA02/TrainneExercicio/Media.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class Media{
+    public static  void main(String[]args){
+        try
+        {
+            float notaOne = Float.parseFloat(JOptionPane.showInputDialog(null, "Entre com a 1° nota"));
+            float notaTwo = Float.parseFloat(JOptionPane.showInputDialog(null, "Entre com a 2° nota"));
+            float notaWork = Float.parseFloat(JOptionPane.showInputDialog(null, "Entre com a nota do trabalho"));
+
+            float media = (notaOne + notaTwo + notaWork)/3;
+
+            String unir = "Notas fornecidas\n" +
+                    "1º ==> " + notaOne +
+                    "\n2º ==> " + notaTwo +
+                    "\nTrabalho ==> " + notaWork +
+                    "\n\nMédia ==> " + media;
+
+            JOptionPane.showMessageDialog(null, unir);
+
+        }catch (NumberFormatException erro){
+            JOptionPane.showMessageDialog(null, "Erro na conversão,\n" +
+                    "Digite apenas caracteres numericos");
+
+        }
+
+    }
+}

18d41538ce3ffc638ca5c5a49edb580e3029dcb5
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-21 13:04:22 -0300

    Create Exercicio5.java

---
+++ TRAINNE/AULA02/TrainneExercicio/Exercicio5.java
@@ -0,0 +1,31 @@
+package ExercicioLuis;
+
+import javax.swing.*;
+
+public class Exercicio5 {
+    public static void main(String[] args){
+        byte idade;
+
+        try {
+            idade = Byte.parseByte(JOptionPane.showInputDialog(null,"Digite sua idade"));
+
+            while(idade < 0){
+                JOptionPane.showMessageDialog(null,
+                        "Idade invalida, tente novamente", "ERRO",JOptionPane.ERROR_MESSAGE);
+                idade = Byte.parseByte(JOptionPane.showInputDialog(null,"Digite sua idade"));
+            }
+
+            if(idade >= 18){
+                JOptionPane.showMessageDialog(null,
+                        "Você tem " + idade + " anos\n" + "Maior de idade", "",JOptionPane.INFORMATION_MESSAGE);
+            }else {
+                JOptionPane.showMessageDialog(null,
+                        "Você tem " + idade + " anos\n" + "Menor de idade", "",JOptionPane.INFORMATION_MESSAGE);
+            }
+
+        }catch (NumberFormatException erro){
+            JOptionPane.showMessageDialog(null, "Erro na conversão,\n" +
+                    "Digite apenas caracteres numericos");
+        }
+    }
+}

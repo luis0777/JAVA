37f4f0b181743541ff08059423f42fd571261b7c
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-22 12:13:43 -0300

    Create ControlandoLoopsContinue.java

---
+++ TRAINNE/AULA03/ExemploControlesLoops/ControlandoLoopsContinue.java
@@ -0,0 +1,15 @@
+package ExemploControlesLoops;
+
+public class ControlandoLoopsContinue {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if (i > 50 && i < 60) {
+                System.out.println(">>" + i + "<<");
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
+
+

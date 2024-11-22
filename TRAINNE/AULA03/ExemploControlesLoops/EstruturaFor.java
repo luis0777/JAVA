0bf34fc148785ee1ecd710a149fb3aefd12a040c
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-22 12:14:58 -0300

    Create EstruturaFor.java

---
+++ TRAINNE/AULA03/ExemploControlesLoops/EstruturaFor.java
@@ -0,0 +1,15 @@
+package ExemploControlesLoops;
+
+public class EstruturaFor {
+    public static void main(String[] args) {
+        // A variável cont pode ser criada aqui
+        // int cont;
+
+        // Ou variável cont pode ser criada dentro do for
+        for (int cont = 1; cont <= 10; cont++) { // cont++  <==> cont=cont+1
+            System.out.println("Numero ==> " + cont);
+        }
+    }
+}
+
+

509a5cd09402ca33a7b592407fe241e858e0c8fe
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-24 00:11:46 -0300

    Create EstruturaWhile.java

---
+++ TRAINNE/EXEMPLOS/ExemplosControlesLoops/EstruturaWhile.java
@@ -0,0 +1,15 @@
+package ExemplosControlesLoops;
+
+public class EstruturaWhile {
+    public static void main(String[] args) {
+
+        int nr = 0;
+
+        while (nr < 10) {
+            System.out.println("Numero ==> " + nr);
+            nr = nr + 1;
+        }
+
+    }
+}
+

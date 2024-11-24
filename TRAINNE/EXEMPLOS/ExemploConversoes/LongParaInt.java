29ec66e9996f6e2944f61b27e008e5d6008da3a4
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-24 00:03:26 -0300

    Create LongParaInt.java

---
+++ TRAINNE/EXEMPLOS/ExemploConversoes/LongParaInt.java
@@ -0,0 +1,10 @@
+package ExemploConversoes;
+
+public class LongParaInt {
+    public static void main(String[] args) {
+        long numeroLong = 123456789L;
+        int numeroInt = (int) numeroLong;
+        System.out.println("long: " + numeroLong + " -> int: " + numeroInt);
+    }
+}
+

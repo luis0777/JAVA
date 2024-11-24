fdb7fb7ec8674234e15dcd2e2beed9b21c264514
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-23 23:59:45 -0300

    Create _05_OperadoresLogicos.java

---
+++ TRAINNE/EXEMPLOS/CodigoFonte/_05_OperadoresLogicos.java
@@ -0,0 +1,13 @@
+package codigoFonte;
+public class _05_OperadoresLogicos {
+
+    public static void main(String[] args) {
+        boolean a = true;
+
+        if (!a) {
+            System.out.println("Saida==> " + a);
+        } else {
+            System.out.println("Saida==> " + !a);
+        }
+    }
+}

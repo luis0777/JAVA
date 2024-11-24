4c73bc1e24b5b4dd766593112e20a350ce70b97f
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-24 00:01:45 -0300

    Create DoubleParaInt.java

---
+++ TRAINNE/EXEMPLOS/ExemploConversoes/DoubleParaInt.java
@@ -0,0 +1,10 @@
+    package ExemploConversoes;
+
+    public class DoubleParaInt {
+        public static void main(String[] args) {
+            double numeroDouble = 123.45;
+            int numeroInt = (int) numeroDouble;
+            System.out.println("double: " + numeroDouble + " -> int: " + numeroInt);
+        }
+    }
+

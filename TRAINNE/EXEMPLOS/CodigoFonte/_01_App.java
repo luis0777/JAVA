12d77a3db40d2043ec4c01d67f522798c0998642
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-23 23:58:35 -0300

    Create _01_App.java

---
+++ TRAINNE/EXEMPLOS/CodigoFonte/_01_App.java
@@ -0,0 +1,10 @@
+package codigoFonte;
+
+public class _01_App {
+    // public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+        // double a= 3.14;
+        // System.out.println(a);
+    }
+}

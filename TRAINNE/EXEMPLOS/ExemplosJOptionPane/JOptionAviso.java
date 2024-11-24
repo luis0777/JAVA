38dfc0a7dc688229c3d1fc44442846c8efdd285a
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-24 16:58:31 -0300

    Create JOptionAviso.java

---
+++ TRAINNE/EXEMPLOS/ExemplosJOptionPane/JOptionAviso.java
@@ -0,0 +1,10 @@
+package  ExemplosJOptionPane;
+import javax.swing.JOptionPane;
+
+public class JOptionAviso {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Este é um aviso importante!", "Aviso", JOptionPane.WARNING_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Erro ao processar a solicitação!", "Erro", JOptionPane.ERROR_MESSAGE);
+    }
+}
+

df77deb2f1caed159084c2b8769f019381eaf9b4
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-24 00:10:55 -0300

    Create escopoDeVariavel.java

---
+++ TRAINNE/EXEMPLOS/ExemplosControlesLoops/escopoDeVariavel.java
@@ -0,0 +1,12 @@
+package ExemplosControlesLoops;
+
+public class escopoDeVariavel {
+    public static void main(String[] args) {
+        int i = 5;
+
+        while (true) {
+            int j =7;
+            System.out.println("Valor de J " + j + "  Valor de i " + i);
+        }        
+    }
+}

3cab67a2cc8d99150aa3179c8896fc98b5a2faf2
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-24 00:03:11 -0300

    Create IntParaString.java

---
+++ TRAINNE/EXEMPLOS/ExemploConversoes/IntParaString.java
@@ -0,0 +1,10 @@
+package ExemploConversoes;
+
+public class IntParaString {
+    public static void main(String[] args) {
+        int numeroInt = 456;
+        String numeroStr = Integer.toString(numeroInt);
+        System.out.println("int: " + numeroInt + " -> String: " + numeroStr);
+    }
+}
+

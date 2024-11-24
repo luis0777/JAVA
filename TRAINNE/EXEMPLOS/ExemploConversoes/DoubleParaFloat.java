a6da66132279513f8ec0b4920f1a4e43499f7650
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-24 00:01:14 -0300

    Create DoubleParaFloat.java

---
+++ TRAINNE/EXEMPLOS/ExemploConversoes/DoubleParaFloat.java
@@ -0,0 +1,10 @@
+package ExemploConversoes;
+
+public class DoubleParaFloat {
+    public static void main(String[] args) {
+        double numeroDouble = 123.456;
+        float numeroFloat = (float) numeroDouble;
+        System.out.println("double: " + numeroDouble + " -> float: " + numeroFloat);
+    }
+}
+

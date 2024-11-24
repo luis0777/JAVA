25f4fce0c933b210fcf5706607f229d6f81aeac0
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-24 00:04:03 -0300

    Create StringParaInt.java

---
+++ TRAINNE/EXEMPLOS/ExemploConversoes/StringParaInt.java
@@ -0,0 +1,10 @@
+package ExemploConversoes;
+
+public class StringParaInt {
+    public static void main(String[] args) {
+        String numeroStr = "123";
+        int numeroInt = Integer.parseInt(numeroStr);
+        System.out.println("String: " + numeroStr + " -> int: " + numeroInt);
+    }
+}
+

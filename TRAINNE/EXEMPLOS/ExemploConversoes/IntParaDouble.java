ae9aeb74828113dbfdecb7a7906f2cc2c56759fe
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-24 00:02:38 -0300

    Create IntParaDouble.java

---
+++ TRAINNE/EXEMPLOS/ExemploConversoes/IntParaDouble.java
@@ -0,0 +1,10 @@
+package ExemploConversoes;
+
+public class IntParaDouble {
+    public static void main(String[] args) {
+        int numeroInt = 100;
+        double numeroDouble = (double) numeroInt;
+        System.out.println("int: " + numeroInt + " -> double: " + numeroDouble);
+    }
+}
+

955d71f3da699052010e59be59919788cb381495
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-24 00:02:55 -0300

    Create IntParaLong.java

---
+++ TRAINNE/EXEMPLOS/ExemploConversoes/IntParaLong.java
@@ -0,0 +1,10 @@
+package ExemploConversoes;
+
+public class IntParaLong {
+    public static void main(String[] args) {
+        int numeroInt = 12345;
+        long numeroLong = (long) numeroInt;
+        System.out.println("int: " + numeroInt + " -> long: " + numeroLong);
+    }
+}
+

a7e6d6aecda8134bb07c1a0bc91ca00b92a26e3d
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-11-23 23:59:28 -0300

    Create _04_OperadoresRelacionais.java

---
+++ TRAINNE/EXEMPLOS/CodigoFonte/_04_OperadoresRelacionais.java
@@ -0,0 +1,12 @@
+package codigoFonte;
+
+public class _04_OperadoresRelacionais {
+
+    public static void main(String[] args) {
+        int a = 10,b=10;
+
+        boolean igual = (a==b);
+        // boolean igual = (a!=b);
+        System.out.println("Saida==> "+igual);
+    }
+}
